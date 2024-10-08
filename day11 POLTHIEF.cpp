2618f12e133513557c517f81c3099bed48557f5b
AnshumanSinghTomar <145200835+AnshumanSinghTomar@users.noreply.github.com>
2024-10-08 21:43:46 +0530

    Add files via upload

---
+++ day11 POLTHIEF.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	
+	int i;
+	cin>>i;
+	while(i--){
+	    int x,y;
+	    
+	    cin>>x>>y;
+	    
+	    if(y>=x)
+	    
+	    cout<<y-x<<endl;
+	    
+	    else 
+	    
+	    cout<<x-y<<endl;
+	}
+	
+	return 0;
+}
