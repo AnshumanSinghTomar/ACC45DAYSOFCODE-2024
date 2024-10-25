dcb5e32404c52cfe5dea08c183ee0e91134d47dc
AnshumanSinghTomar <145200835+AnshumanSinghTomar@users.noreply.github.com>
2024-10-25 23:32:10 +0530

    Add files via upload

---
+++ day 23 RECENTCONT.py
@@ -0,0 +1,7 @@
+# cook your dish here
+for i in range(int(input())):
+    n = int(input())
+    a = list(map(str,input().split()))
+    b = a.count('START38')
+    c = a.count('LTIME108')
+    print(b,c)
