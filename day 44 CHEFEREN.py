5d597b3f3e40a2730ee8108312bef64399ca7315
AnshumanSinghTomar <145200835+AnshumanSinghTomar@users.noreply.github.com>
2024-11-15 21:38:44 +0530

    Add files via upload

---
+++ day 44 CHEFEREN.py
@@ -0,0 +1,12 @@
+# cook your dish here
+t = int(input())
+
+for i in range(t):
+    ans = 0 
+    n,a,b = map(int,input().split())
+    for j in range(1,n+1):
+        if j%2 == 0:
+            ans+=a
+        else:
+            ans+=b
+    print(ans)
